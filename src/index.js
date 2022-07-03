ff7a5b6f6855088f9f307269a2731e4d9beeb5ad
dbataeva <b.daffa.v@yandex.ru>
2022-07-03 10:17:43 +0300

    init: create reverseInt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let numAsStr;
+    let reverseStr = '';
+
+    if (n < 0) {
+        numAsStr = (-n).toString();
+    } else {
+        numAsStr = n.toString();
+    }
+
+    for (let i = numAsStr.length - 1; i >= 0; --i) {
+        reverseStr += numAsStr[i];
+    }
+
+    return (+reverseStr);
 }
